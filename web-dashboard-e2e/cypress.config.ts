08f07c2db974d372d42c389493f2d36c4d34e1a4
ariefprasetyo939 <arief.prasetyo911@outlook.com>
2025-08-14 14:50:03 +0700

    Update: initial commit

---
+++ web-dashboard-e2e/cypress.config.ts
@@ -0,0 +1,20 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'npx nx run @bug-insight-react-nestjs/web-dashboard:dev',
+        production:
+          'npx nx run @bug-insight-react-nestjs/web-dashboard:preview',
+      },
+      ciWebServerCommand:
+        'npx nx run @bug-insight-react-nestjs/web-dashboard:preview',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
