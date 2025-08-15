62d91c6bef024abe9cb286cb9552defed32b9e20
ariefprasetyo939 <arief.prasetyo911@outlook.com>
2025-08-15 10:03:08 +0700

    Update: add Swagger into api-gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -6,11 +6,28 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+
+  const globalPrefix = 'api/v1/';
   app.setGlobalPrefix(globalPrefix);
+  
+  //swagger config
+  const config = new DocumentBuilder();
+  config.setTitle('API Gateway');
+  config.setDescription('API Gateway documentation');
+  config.setVersion('1.0');
+  config.addBearerAuth(); //because we'll be using JWT auth
+  config.build();
+
+  const document = SwaggerModule.createDocument(app, config.build());
+  SwaggerModule.setup('api/documentation', app, document);
+
+  //pipe validation
+  app.useGlobalPipes();
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
